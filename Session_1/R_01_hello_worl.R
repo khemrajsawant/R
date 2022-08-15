fec687be84b5d1c20985df39940e0efdbb69a359
Khemraj SAWANTMORYE <31571407+khemrajsawant@users.noreply.github.com>
2022-08-15 15:02:41 +0530

    first commit

---
+++ Session_1/R_01_hello_worl.R
@@ -0,0 +1,38 @@
+# R program to illustrate
+# printing output of an R program
+
+# print string
+print("GFG")
+
+# print variable
+# it will print 'GeeksforGeeks' on the console
+x <- "GeeksforGeeks"
+print(x)
+
+# R program to illustrate
+# printing output of an R program
+
+x <- "GeeksforGeeks"
+
+# using paste inside print()
+print(paste(x, "is best (paste inside print())"))
+
+# using paste0 inside print()
+print(paste0(x, "is best (paste0 inside print())"))
+# R program to illustrate
+# printing output of an R program
+
+x = "GeeksforGeeks" # string
+x1 = 255		 # integer
+x2 = 23.14		 # float
+
+# string print
+sprintf("%s is best", x)
+
+# integer print
+sprintf("%d is integer", x1)
+
+# float print
+sprintf("%f is float", x2)
+
+
